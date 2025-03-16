f48c5d06a88f5a00f7114d6191b8d007aa53fb62
Anirudh1103 <cmanirudh03@gmail.com>
2025-03-16 23:07:01 +0530

    Packages completed, Notes not yet added

---
+++ 12 Object Oriented Programming/02 Packages, Static, SIngleton Class In-built Methods/Packages/Main.java
@@ -0,0 +1,9 @@
+package com.anirudh.lecture2;
+
+import static com.anirudh.MathOperations.Operations.sum;
+
+public class Main {
+    public static void main(String[] args) {
+        sum();
+    }
+}
---
+++ 12 Object Oriented Programming/02 Packages, Static, SIngleton Class In-built Methods/Packages/MathOperations/Operations.java
@@ -0,0 +1,9 @@
+package com.anirudh.MathOperations;
+
+public class Operations {
+    public static void sum()
+    {
+        System.out.println("I don't know how to sum");
+    }
+}
+ 
